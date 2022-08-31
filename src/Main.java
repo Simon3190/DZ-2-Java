7f5a811e028a171a6670392f1ec19c64cd312cd0
Simon31 <saimon3190@yandex.ru>
2022-08-31 09:47:45 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import org.w3c.dom.ls.LSOutput;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int ticket = 100; // Стоимость билета
+        int money = 20; // Цена 1 мили
+
+        System.out.println(ticket / money);
+
+        // Выводим расчет бонусных миль.
+
+    }
+
+
+}
